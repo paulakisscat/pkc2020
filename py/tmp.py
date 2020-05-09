8098ff147cb47ba868f77480936ce6e0e5b103d5
liyubin <liyubin@forchange.tech>
2020-05-09 16:50:48 +0800

    creeper0

---
+++ py/tmp.py
@@ -0,0 +1,41 @@
+#！爬虫
+
+import requests
+
+# res = requests.get('https://localprod.pandateacher.com/python-manuscript/crawler-html/sanguo.md')
+#
+# print(type(res))
+#
+# print(res.status_code)
+
+
+# res = requests.get('https://res.pandateacher.com/2018-12-18-10-43-07.png')
+#
+# # 返回的二进制形式图片内容
+# pic = res.content
+#
+# # 图片内容以二进制wb读写
+# photo = open('chong.jpg','wb')
+#
+# print(type(photo))
+#
+# photo.write(pic)
+#
+# photo.close()
+
+
+res = requests.get('https://localprod.pandateacher.com/python-manuscript/crawler-html/sanguo.md')
+
+res.encoding='utf-8'
+
+novel = res.text
+
+三国演义 = open('pani.txt','a+')
+
+三国演义.write(novel)
+
+三国演义.close()
+
+
+
+
